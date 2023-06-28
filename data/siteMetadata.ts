e365934f21cabcffd376067198f44f73bcad5ef0
Ram Krishna Singh <rkscodes@gmail.com>
2023-06-28 23:58:43 +0530

    :sparkles: chore: added umamai analytics

--- data/siteMetadata.ts
+++ data/siteMetadata.ts
@@ -22,7 +22,7 @@ export let siteMetadata = {
   analytics: {
     plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
     simpleAnalytics: false, // true or false
-    umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
+    umamiWebsiteId: '50b0142b-a6cb-49af-886b-f960e54ed683', // e.g. 123e4567-e89b-12d3-a456-426614174000
     googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
   },
   socialAccounts: {
